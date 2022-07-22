41f70aaae06cb7a04c8b95736192dbf55602bf4e
erhnaks <erhnaks@hotmail.com>
2022-07-22 10:49:48 +0100

    added a second method of using error hadnling

--- error_handling/app.js
+++ error_handling/app.js
@@ -1,21 +1,35 @@
 const express = require('express');
 const app = express();
 
-app.listen(8080);
+app.listen(4000);
+
+// Below Line is also working I commmented it to try differen method....
+
+// app.get('/error', (req, res, next) => {
+//   const err = new Error('Something went wrong ');
+//   next(err);
+// });
+
+// const errorLogger = (err, req, res, next) => {
+//   console.log(err.stack);
+//   next(err);
+// }
+// app.use(errorLogger);
+
+// const sendError = (err, req, res) => {
+//   res.status(500).send(err.message);
+// }
+// app.use(sendError);
 
 app.get('/error', (req, res, next) => {
-  const err = new Error('Something went wrong ');
+  next(Error('Something went wrong'));
+})
+
+app.use((err, req, res, next) => {
+  console.log(err.stack);
   next(err);
 });
 
-const errorLogger = (err, req, res, next) => {
-  console.log(err.stack);
-  next(err);
-}
-app.use(errorLogger);
-
-const sendError = (err, req, res) => {
+app.use((err, req, res, next) => {
   res.status(500).send(err.message);
-}
-app.use(sendError);
-
+});
