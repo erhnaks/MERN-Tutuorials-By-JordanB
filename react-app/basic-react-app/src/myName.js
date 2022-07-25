f053d2b5fc9a9c0c43f25435f641725428e2a0cd
erhnaks <erhnaks@hotmail.com>
2022-07-25 12:24:57 +0100

    exercise 2

---
+++ react-app/basic-react-app/src/myName.js
@@ -0,0 +1,26 @@
+import './myName.css';
+
+
+const name = () => {
+
+  const myName = {
+  firstName: 'John',
+  surname: 'Doe',
+  age: 14,
+  location:'London'
+}
+
+return (
+<div className='myName'>
+<h1>{myName.firstName}</h1>
+<h1>{myName.surname}</h1>
+<h1>{myName.age}</h1>
+<h1>{myName.location}</h1>
+
+</div>
+
+
+);
+}
+
+export default name;
