48f94952b0d732a52bb24d263e59d632a6fba07a
erhnaks <erhnaks@hotmail.com>
2022-07-25 12:55:27 +0100

    adding name to react and printing in the browser

--- react-app/basic-react-app/src/myName.js
+++ react-app/basic-react-app/src/myName.js
@@ -1,26 +1,21 @@
-import './myName.css';
-
+import "./myName.css";
 
 const name = () => {
-
   const myName = {
-  firstName: 'John',
-  surname: 'Doe',
-  age: 14,
-  location:'London'
-}
+    firstName: "John",
+    surname: "Doe",
+    age: 14,
+    location: "London",
+  };
 
-return (
-<div className='myName'>
-<h1>{myName.firstName}</h1>
-<h1>{myName.surname}</h1>
-<h1>{myName.age}</h1>
-<h1>{myName.location}</h1>
-
-</div>
-
-
-);
-}
+  return (
+    <div className="myName">
+      <h1>{myName.firstName}</h1>
+      <h1>{myName.surname}</h1>
+      <h1>{myName.age}</h1>
+      <h1>{myName.location}</h1>
+    </div>
+  );
+};
 
 export default name;
