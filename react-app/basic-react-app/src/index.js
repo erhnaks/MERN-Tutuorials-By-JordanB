8c7c47aae2bd1cf485aa6da9ef4cb04d49625cbf
erhnaks <erhnaks@hotmail.com>
2022-07-27 11:13:12 +0100

    Hooks useEffect

--- react-app/basic-react-app/src/index.js
+++ react-app/basic-react-app/src/index.js
@@ -21,8 +21,8 @@ import Greet from "./hooks/Greet";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <LessText />
-    <Greet /> */}
+    <LessText />
+    <Greet />
 
     {/* <Shopping /> */}
     {/* <Car />
