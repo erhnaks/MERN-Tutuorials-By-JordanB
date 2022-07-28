1442d87abaad117645e24ac3dde4b0717e971b4e
erhnaks <erhnaks@hotmail.com>
2022-07-28 10:30:58 +0100

    modified

--- react-app/basic-react-app/src/index.js
+++ react-app/basic-react-app/src/index.js
@@ -24,7 +24,7 @@ import Example from "./state/Example";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <NotDog />
+    {/* <NotDog /> */}
     <Example/>
 
     {/* <LessText text='Heyyo '  />
